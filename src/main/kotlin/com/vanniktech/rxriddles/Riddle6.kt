77a4443a438998fb75a36e8794e17eab5aec5418
Nenad Nenadovic <neno.nenadovic@gmail.com>
2021-07-31 09:14:47 +0200

    Solve riddle 6

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle6.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle6.kt
@@ -1,16 +1,18 @@
 package com.vanniktech.rxriddles
 
 import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.functions.BiFunction
 
 object Riddle6 {
-  /**
-   * Execute both [first] and [second] Single's in parallel and provide both results as a pair.
-   * Assume both [first] and [second] will execute on a different thread already.
-   * This is a slightly simpler version of [Riddle102], where no schedulers are applied by default.
-   *
-   * Use case: Execute two network requests in parallel and wait for each other and process the combined data.
-   */
-  fun solve(first: Single<Int>, second: Single<Int>): Single<Pair<Int, Int>> {
-    TODO()
-  }
+    /**
+     * Execute both [first] and [second] Single's in parallel and provide both results as a pair.
+     * Assume both [first] and [second] will execute on a different thread already.
+     * This is a slightly simpler version of [Riddle102], where no schedulers are applied by default.
+     *
+     * Use case: Execute two network requests in parallel and wait for each other and process the combined data.
+     */
+    fun solve(first: Single<Int>, second: Single<Int>): Single<Pair<Int, Int>> {
+        return Single.zip(first, second, BiFunction<Int, Int, Pair<Int, Int>> { t1, t2 -> Pair(t1, t2) })
+        //return Single.zip(first, second, ((Int, Int) -> Pair<Int, Int>) { t1, t2 -> Pair(t1, t2) })
+    }
 }
