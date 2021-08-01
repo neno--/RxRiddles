30ae1575b8c68cee90358f0a75007bfdb9a89362
Nenad Nenadovic <neno.nenadovic@gmail.com>
2021-08-01 21:03:06 +0200

    Solve riddles 15 and 16

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle15.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle15.kt
@@ -3,12 +3,11 @@ package com.vanniktech.rxriddles
 import io.reactivex.rxjava3.core.Observable
 
 object Riddle15 {
-  /**
-   * Concatenate the [first] Observable with the [second] while subscribing to both early.
-   *
-   * Use case: You have two sources of your data (cache & network request). You want to subscribe to both right away and keep the emission order.
-   */
-  fun solve(first: Observable<Int>, second: Observable<Int>): Observable<Int> {
-    TODO()
-  }
+    /**
+     * Concatenate the [first] Observable with the [second] while subscribing to both early.
+     *
+     * Use case: You have two sources of your data (cache & network request). You want to subscribe to both right away and keep the emission order.
+     */
+    fun solve(first: Observable<Int>, second: Observable<Int>): Observable<Int> =
+        Observable.concatEager(listOf(first, second))
 }
