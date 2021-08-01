8c90bb3b4984be2d57a76fbe7e22aefd066c8e51
Nenad Nenadovic <neno.nenadovic@gmail.com>
2021-08-01 13:55:48 +0200

    Solve riddles 14

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle14.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle14.kt
@@ -3,12 +3,11 @@ package com.vanniktech.rxriddles
 import io.reactivex.rxjava3.core.Single
 
 object Riddle14 {
-  /**
-   * Try the given [source] up to three times unless an [IllegalArgumentException] has been emitted.
-   *
-   * Use case: Retry an operation for a number of times or until a valid error occurred.
-   */
-  fun solve(source: Single<Unit>): Single<Unit> {
-    TODO()
-  }
+    /**
+     * Try the given [source] up to three times unless an [IllegalArgumentException] has been emitted.
+     *
+     * Use case: Retry an operation for a number of times or until a valid error occurred.
+     */
+    fun solve(source: Single<Unit>): Single<Unit> =
+        source.retry(2) { t -> t !is IllegalArgumentException }
 }
