9c2f9a5231929d276c9e26f37dd15944a181a4b8
Nenad Nenadovic <neno.nenadovic@gmail.com>
2021-08-05 08:20:50 +0200

    Solve riddles 22, 23, and 24

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle23.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle23.kt
@@ -3,12 +3,10 @@ package com.vanniktech.rxriddles
 import io.reactivex.rxjava3.core.Observable
 
 object Riddle23 {
-  /**
-   * Cast each emission of the [source] from [Any] to [String].
-   *
-   * Use case: You get some data from a bad source and know for sure it's of a certain type that you require.
-   */
-  fun solve(source: Observable<Any>): Observable<String> {
-    TODO()
-  }
+    /**
+     * Cast each emission of the [source] from [Any] to [String].
+     *
+     * Use case: You get some data from a bad source and know for sure it's of a certain type that you require.
+     */
+    fun solve(source: Observable<Any>): Observable<String> = source.map(Any::toString)
 }
