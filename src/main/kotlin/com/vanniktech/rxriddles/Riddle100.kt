f9839c848d19db26e9202f1c7ce05b65c6f10383
Nenad Nenadovic <neno.nenadovic@gmail.com>
2021-08-12 22:44:11 +0200

    Solve riddles 37 and 100

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle100.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle100.kt
@@ -1,15 +1,18 @@
 package com.vanniktech.rxriddles
 
 import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.TimeUnit
 
 object Riddle100 {
-  /**
-   * If the [source] emits more than once within 300ms we want to emit [Unit].
-   * If there is only one or non emissions within 300ms we don't want to emit anything.
-   *
-   * Use case: Double click detection mechanism for a button.
-   */
-  fun solve(source: Observable<Unit>): Observable<Unit> {
-    TODO()
-  }
+    /**
+     * If the [source] emits more than once within 300ms we want to emit [Unit].
+     * If there is only one or non emissions within 300ms we don't want to emit anything.
+     *
+     * Use case: Double click detection mechanism for a button.
+     */
+    fun solve(source: Observable<Unit>): Observable<Unit> {
+        return source
+            .buffer(300, 0, TimeUnit.MILLISECONDS)
+            .flatMap { list -> if (list.size > 1) Observable.just(list.get(0)) else Observable.empty() }
+    }
 }
