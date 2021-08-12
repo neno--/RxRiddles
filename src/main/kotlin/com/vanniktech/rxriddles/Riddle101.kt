b1f33efa3751db351329607d80c170fac38f3df3
Nenad Nenadovic <neno.nenadovic@gmail.com>
2021-08-12 22:44:32 +0200

    Solve riddles 101 and 102

--- src/main/kotlin/com/vanniktech/rxriddles/Riddle101.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle101.kt
@@ -1,15 +1,16 @@
 package com.vanniktech.rxriddles
 
 import io.reactivex.rxjava3.core.Observable
+import java.util.concurrent.TimeUnit
 
 object Riddle101 {
-  /**
-   * Create an Observable that acts as a countdown. Counting down from number of [seconds] to 0
-   * while emitting the number of seconds remaining each time.
-   *
-   * Use case: You have some countdown functionality and want to display how many seconds are left.
-   */
-  fun solve(seconds: Long): Observable<Long> {
-    TODO()
-  }
+    /**
+     * Create an Observable that acts as a countdown. Counting down from number of [seconds] to 0
+     * while emitting the number of seconds remaining each time.
+     *
+     * Use case: You have some countdown functionality and want to display how many seconds are left.
+     */
+    fun solve(seconds: Long): Observable<Long> =
+        Observable.intervalRange(0, seconds + 1, 0, 1, TimeUnit.SECONDS)
+            .map { seconds - it }
 }
--- src/main/kotlin/com/vanniktech/rxriddles/Riddle102.kt
+++ src/main/kotlin/com/vanniktech/rxriddles/Riddle102.kt
@@ -1,14 +1,23 @@
 package com.vanniktech.rxriddles
 
 import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.functions.BiFunction
+import io.reactivex.rxjava3.schedulers.Schedulers
+
 
 object Riddle102 {
-  /**
-   * Execute both [first] and [second] Single's in parallel and provide both results as a pair.
-   *
-   * Use case: Execute two network requests in parallel and wait for each other and process the combined data.
-   */
-  fun solve(first: Single<Int>, second: Single<Int>): Single<Pair<Int, Int>> {
-    TODO()
-  }
+    /**
+     * Execute both [first] and [second] Single's in parallel and provide both results as a pair.
+     *
+     * Use case: Execute two network requests in parallel and wait for each other and process the combined data.
+     */
+    fun solve(first: Single<Int>, second: Single<Int>): Single<Pair<Int, Int>> {
+
+        val x: (Int, Int) -> Pair<Int, Int> = { f, s -> f to s }
+        val y: (Int, Int) -> Int = fun(a: Int, b: Int): Int = 2
+        val z: BiFunction<Int, Int, Int> = BiFunction { f, s -> f + s }
+        val solution: BiFunction<Int, Int, Pair<Int, Int>> = BiFunction { f, s -> f to s }
+
+        return Single.zip(first.subscribeOn(Schedulers.computation()), second.subscribeOn(Schedulers.computation()), solution)
+    }
 }
